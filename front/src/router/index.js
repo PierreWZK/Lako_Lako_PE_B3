c61655267b733da5e1e7e484537e93fc94b7b47f
PierreWZK <pierredolivet@live.fr>
2023-02-03 16:39:49 +0100

    feat/fix(front): 🔥  Pierre - pull header_creation

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -16,6 +16,12 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
+  {
+    path: "/login",
+    name: "login",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Login.vue"),
+  },
 ];
 
 const router = createRouter({
@@ -24,3 +30,15 @@ const router = createRouter({
 });
 
 export default router;
+
+// import Vue from 'vue'
+// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// // Import Bootstrap and BootstrapVue CSS files (order is important)
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// // Make BootstrapVue available throughout your project
+// Vue.use(BootstrapVue)
+// // Optionally install the BootstrapVue icon components plugin
+// Vue.use(IconsPlugin)
