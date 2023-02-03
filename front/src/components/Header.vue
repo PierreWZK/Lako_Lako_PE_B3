9eb710dbbcd73f814e52f114ea97be9106ede483
Erlé Duval <erle.duval@yahoo.com>
2023-02-03 16:37:48 +0100

    feat(front): :fire: Ajout de la page Principale

---
+++ front/src/components/Header.vue
@@ -0,0 +1,40 @@
+/* eslint-disable */
+<template>
+    <h1>{{ msg }}</h1>
+    <div class="header">
+        <ul>
+            <li>
+                logo
+            </li>
+            <li>rechercher</li>
+            <li>articles</li>
+            <li>
+                Compte
+            </li>
+            <li>Panier</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+
+</script>
+
+<style>
+.header{
+    display: flex;
+    background-color: lightcoral;
+}
+ul{
+    display: flex;
+    background-color: lightblue;
+    width: 100%;
+}
+</style>
