d95cbb4939cce96e3a55775a0cbac0de7a5c7b42
Pierre <pierredolivet@live.fr>
2023-05-11 17:09:27 +0200

    feat(front): 🔥 Footer terminé (visuel seulement)

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,6 +1,9 @@
 <!-- eslint-disable -->
 <template>
   <Header msg=""/>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
   <Footer msg=""/>
 </template>
 
@@ -8,10 +11,13 @@
 import Header from './components/HeaderTemplate.vue'
 import Footer from './components/FooterTemplate.vue'
 
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
   name: 'App',
   components: { 
     Header,
+    HelloWorld,
     Footer
   }
 }
